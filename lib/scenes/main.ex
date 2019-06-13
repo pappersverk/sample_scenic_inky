4d549ca3e4554cdc622a71f5a2e39c0261a553fe
Lars Wikman <lars.wikman@gmail.com>
2019-06-13 15:03:08 +0200

    Clean working sample that looks alright.

--- lib/scenes/main.ex
+++ lib/scenes/main.ex
@@ -5,10 +5,11 @@ defmodule SampleScenicInky.Scene.Main do
   import Scenic.Primitives
   require Logger
 
-  @graph Graph.build(font_size: 22, font: :roboto_mono, theme: :light)
-         |> rectangle({212, 4}, fill: :black)
-         |> rectangle({212, 4}, fill: :red)
-         |> text("ViewPort")
+  @graph Graph.build(font_size: 32, font: :roboto_mono, theme: :light)
+         |> rectangle({212, 104}, fill: :white)
+         |> text("Inky", fill: :black, t: {8, 26})
+         |> text("Scenic", fill: :red, t: {8, 60})
+         |> text("Nerves", fill: :black, t: {8, 94})
 
   def init(_, _) do
     state = %{
@@ -16,20 +17,20 @@ defmodule SampleScenicInky.Scene.Main do
       count: 0
     }
 
-    Process.send_after(self(), :update, 50000)
+    Process.send_after(self(), :update, 30000)
     {:ok, state, push: @graph}
   end
 
   def handle_info(:update, state) do
     graph =
-      Graph.build(font_size: 22, font: :roboto_mono, theme: :light)
-      |> rectangle({212, 4}, fill: :black)
-      |> rectangle({4, 104}, fill: :red)
-      |> text(to_string(state.count))
+      Graph.build(font_size: 32 + state.count, font: :roboto_mono, theme: :light)
+      |> rectangle({212, 104}, fill: :white)
+      |> text("Inky", fill: :black, t: {8, 26})
+      |> text("Scenic", fill: :red, t: {8, 60})
+      |> text("Nerves", fill: :black, t: {8, 94})
 
-    Process.send_after(self(), :update, 50000)
+    Process.send_after(self(), :update, 30000)
     count = state.count + 1
-    Logger.info(to_string(count))
     {:noreply, %{state | count: count, graph: graph}, push: graph}
   end
 end
